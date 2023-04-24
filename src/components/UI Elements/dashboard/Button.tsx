6ccd4d30de1279424684ad165f4fc6712dadd2b4
VeldinS <190302158@student.ius.edu.ba>
2023-04-24 10:08:16 +0200

    student dashboard added

---
+++ src/components/UI Elements/dashboard/Button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './button.css'
+
+interface DashboardButtonProps{
+    field1: string,
+    field2: string
+}
+
+function Button(props: DashboardButtonProps) {
+    return (
+        <div>
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <p className="title">{props.field1}</p>
+                    </div>
+                    <div className="flip-card-back">
+                        <p className="title">{props.field2}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Button;
--- src/components/student-dashboard/StudentDashboard.tsx
+++ src/components/student-dashboard/StudentDashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from "../UI Elements/navigation/Navigation";
 
 import './studentDashboard.css'
+import Button from "../UI Elements/dashboard/Button";
 
 function StudentDashboard() {
     return (
@@ -10,10 +11,29 @@ function StudentDashboard() {
                 <Navigation/>
                 <div className={"dashboard-heading"}>
                     <h1>STUDENT DASHBOARD</h1>
+                    <div className={"dashboard-buttons"}>
+                        <div className={"button"}>
+                             <Button field1={'ACADEMIC RECORD'} field2={'Check your detailed academic record!'}/>
+                        </div>
+                        <div className={"button"}>
+                            <Button field1={'COURSE SCHEDULE'} field2={'Check your course schedule!'}/>
+                        </div>
+                        <div className={"button"}>
+                            <Button field1={'EXAM SCHEDULE'} field2={'Check if there are any new exam schedules!'}/>
+                        </div>
+                        <div className={"button"}>
+                            <Button field1={'GRADE DETAILS'} field2={'Check your grade details!'}/>
+                        </div>
+                        <div className={"button"}>
+                            <Button field1={'YOUR PERFORMANCE'} field2={'Analyse and check your performance!'}/>
+                        </div>
+                        <div className={"button"}>
+                            <Button field1={'STUDENT CERTIFICATE APPLICATION'} field2={'Request a new application document!'}/>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            </div>
+        </div>
     );
 }
 
